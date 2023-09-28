90371465250b7238e4d7011f924eaa85490b6268
AyishaSidviqua <ayisha.siddika@zealeye.com>
2023-09-28 11:58:18 +0530

    demo

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Calculator from './Module/Calculator/Calculator';
+
+function App() {
+  return (
+    <div >
+        <Calculator/>
+    </div>
+  );
+}
+
+export default App;
